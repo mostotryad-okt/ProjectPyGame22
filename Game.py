cebc2707074b2c1a3fd0b024859686093f113b3c
SlavaPetrov2008 <SlavaPetrov2008@yandex.ru>
2023-01-06 20:15:13 +0300

    итог 2

--- Game.py
+++ Game.py
@@ -24,27 +24,36 @@ def terminate():
     pygame.quit()
     sys.exit()
 
+def render_start_screen(screen, x1, x2, image_bear, image_pine):
+    screen.blit(image_bear, (x1, int(WINDOW_HEIGHT * 0.8)))
+    screen.blit(image_pine, (x2, int(WINDOW_HEIGHT * 0.8)))
+    if x1 >= WINDOW_WIDTH:
+        x1 = -SPEED * 2
+    if x2 >= WINDOW_WIDTH:
+        x2 = -SPEED * 2
+    return x1 + SPEED * 2, x2 + SPEED * 2
+
 
 def start_screen(screen):
-    intro_text = ["Проект",
-                  "Выполнил:",
-                  "Петоров Вячеслав",
-                  "9а"]
+    intro_text = ["Мишка косолапый по лесу идет,",
+                  "Шишки собирает, песенки поет.",
+                  "Вдруг упала шишка, прямо мишке в лоб,",
+                  "Мишка оступился и об землю хлоп!"]
 
     fon = pygame.transform.scale(load_image('фон.png'), (WINDOW_WIDTH, WINDOW_HEIGHT))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, int(30 * WINDOW_RATIO))
-    text_coord = 50
+    font = pygame.font.Font(None, int(35 * WINDOW_RATIO))
+    text_coord = 460 * WINDOW_RATIO
     for line in intro_text:
-        string_rendered = font.render(line, 1, (10, 30, 30))
+        string_rendered = font.render(line, 1, (0, 191, 255))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = WINDOW_WIDTH // 2 - string_rendered.get_width() // 2
         text_coord += intro_rect.height
         text_w = string_rendered.get_width()
         text_h = string_rendered.get_height()
-        pygame.draw.rect(screen, (10, 180, 180), (intro_rect[0] - 5, intro_rect[1] - 5, intro_rect[2] + 10, intro_rect[3] + 10))
+        # pygame.draw.rect(screen, (255, 255, 255), (intro_rect[0] - 5, intro_rect[1] - 5, intro_rect[2] + 10, intro_rect[3] + 10))
         screen.blit(string_rendered, intro_rect)
 
 
@@ -66,7 +75,7 @@ def show_message(screen, message, flag, message2 = '100', message3 = '100'):
         interval += distance
         screen.blit(text, (text_x, text_y))
 
-        text = font.render('RSN: ' + message3 + '%', 1, (40, 75, 25))
+        text = font.render('LVL: ' + str(100 - int(message3)) + '%', 1, (40, 75, 25))
         text_x = WINDOW_WIDTH // 40
         text_y = WINDOW_HEIGHT * interval
         interval += distance
@@ -192,7 +201,7 @@ class Hero(pygame.sprite.Sprite):
 
 
 def show_game_over(screen, message, score):
-    intro_text = [score]
+    intro_text = ['Score: ' + score]
 
     fon = pygame.transform.scale(load_image('фон.png'), (WINDOW_WIDTH, WINDOW_HEIGHT))
     # screen.blit(fon, (0, 0))
@@ -222,6 +231,37 @@ def show_game_over(screen, message, score):
     screen.blit(text, (text_x, text_y))
 
 
+def show_game_victory(screen, message, score):
+    intro_text = ['Score: ' + score]
+
+    fon = pygame.transform.scale(load_image('фон.png'), (WINDOW_WIDTH, WINDOW_HEIGHT))
+    # screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, int(100 * WINDOW_RATIO))
+    text_coord = 300 * WINDOW_RATIO
+    for line in intro_text:
+        string_rendered = font.render(line, 1, (0, 191, 255))
+        intro_rect = string_rendered.get_rect()
+        text_w = string_rendered.get_width()
+        text_h = string_rendered.get_height()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = WINDOW_WIDTH // 2 - text_w // 2
+        text_coord += intro_rect.height
+        pygame.draw.rect(screen, (40, 50, 40),
+                         (intro_rect[0] - 5, intro_rect[1] - 5, intro_rect[2] + 10, intro_rect[3] + 10), 5)
+        screen.blit(string_rendered, intro_rect)
+
+    font = pygame.font.Font(None, int(100 * WINDOW_RATIO))
+    text = font.render(message, 1, (0, 191, 255))
+    text_x = WINDOW_WIDTH // 2 - text.get_width() // 2
+    text_y = WINDOW_HEIGHT * 0.5 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    pygame.draw.rect(screen, (40, 50, 40), (text_x - 10, text_y - 10,
+                                            text_w + 20, text_h + 20), 5)
+    screen.blit(text, (text_x, text_y))
+
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -245,6 +285,11 @@ def main():
     image_pine = load_image("shishka2.png")
     image_pine = pygame.transform.scale(image_pine, (70 * WINDOW_RATIO, 70 * WINDOW_RATIO))
     image_pine = pygame.transform.flip(image_pine, 0, 1)
+    image_pine_start = pygame.transform.rotate(image_pine, 90)
+    image = load_image("bear.png")
+    image_bear = pygame.transform.scale(image, (125 * WINDOW_RATIO, 125 * WINDOW_RATIO))
+    x_start_bear = 100 * WINDOW_RATIO
+    x_start_pine = 0
 
     hero = Hero()
     global HLTH, RSN
@@ -255,8 +300,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            if HLTH == 0:
-                flag = False
             if event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -279,9 +322,12 @@ def main():
                 if random.choice([0, 1, 0, 0]):
                     Pine(hero.get_position()[0] + hero.width // 2, random.randint(1, WINDOW_HEIGHT // 2), image_pine, pins)
                     RSN -= 1
+                    RSN = max(RSN, 0)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     flag = False
+        if RSN == 0 or HLTH == 0:
+            flag = False
         if flag:
             screen.fill(pygame.Color('black'))
             screen.blit(image_background, (0, 0))
@@ -298,10 +344,15 @@ def main():
             show_message(screen, str(score), flag, message2=str(HLTH), message3=str(RSN))
         else:
             if HLTH == 0:
+                screen.blit(image_background, (0, 0))
                 show_game_over(screen, 'Game Over', str(score))
+            elif RSN == 0:
+                show_game_victory(screen, 'Victory', str(score))
             else:
                 start_screen(screen)
                 show_message(screen, str(score), flag)
+                x_start_bear, x_start_pine = render_start_screen(screen, x_start_bear, x_start_pine,
+                                                                 image_bear, image_pine_start)
 
         pygame.display.flip()
         clock.tick(FPS)
